398d6ee1014555d75dfcb6b9e9743ab2528df293
jmv-support <78544912+jmv-support@users.noreply.github.com>
2021-02-08 22:51:06 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -33,6 +33,8 @@ function et_get_footer_credits() {
   $credits_format = '<%2$s id="footer-info">%1$s</%2$s>';
 
   $footer_credits = "Copyright &copy; " . date_i18n('Y') . " " . et_get_option( 'custom_footer_credits', '' );
+    <?php echo get_bloginfo( 'name' ); ?>
+<a href="https://www.google.com">Google</a>
 
   if ( '' === trim( $footer_credits ) ) {
     return et_get_safe_localization( sprintf( $credits_format, $original_footer_credits, 'p' ) );
